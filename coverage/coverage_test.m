31eee6373a67ef1e369803bdc0779992fb0973bc
YinTat <YinTatLee@gmail.com>
2021-12-17 15:58:59 -0800

    update test

--- coverage/coverage_test.m
+++ coverage/coverage_test.m
@@ -1,7 +1,7 @@
 debug = 0;
 problems = [];
-folders = {'basic', 'metabolic', 'netlib', 'extra'};
+folders = {'basic', 'metabolic', 'netlib', 'extra', 'netlib_extra'};
 
 presolve_test(debug, folders, problems)
-%uniform_sample_test(debug, folders, problems, 200)
-%p_value_test();
+uniform_sample_test(debug, folders, problems, 200)
+p_value_test();
