375fb76798573d944bb206ec5d7b5507fd9716b7
Santosh Vempala <vempala@cc.gatech.edu>
2023-06-22 09:54:52 -0400

    Update coverage_test.m

--- coverage/coverage_test.m
+++ coverage/coverage_test.m
@@ -6,7 +6,7 @@ presolve_test(debug, folders, problems)
 
 folders = {'basic', 'metabolic', 'netlib'};
 sample_test(debug, folders, problems, 100, 'uniform')
-sample_test(debug, folders, problems, 100, 'expoential')
+sample_test(debug, folders, problems, 100, 'exponential')
 sample_test(debug, folders, problems, 100, 'normal')
 
 
